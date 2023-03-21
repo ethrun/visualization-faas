2c88baa0ae39a94a75aaba865f46e705db36f94a
stark-eth <1319114370@qq.com>
2023-03-22 01:12:29 +0800

    init commit

---
+++ handler/handler.go
@@ -0,0 +1,37 @@
+package handler
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Excute(c *gin.Context) {
+	resp := make(map[string]interface{})
+	resp["message"] = "suc"
+	database := c.Query("database")
+	excute := c.Query("excute")
+	db, err := sql.Open("mysql", database)
+	if err != nil {
+		fmt.Println("Error executing connect:", err)
+		resp["message"] = err
+		return
+	}
+	res, err := db.Exec(excute)
+
+	if err != nil {
+		fmt.Printf("Error happened in JSON marshal. Err: %s", err)
+		c.Status(http.StatusNotFound)
+	} else {
+		resp["resulte"] = res
+	}
+	c.JSON(http.StatusOK, resp)
+	defer db.Close()
+}
+
+func Cors(c *gin.Context) {
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.Next()
+}
